7672df071f85fefc56f68f89a656d768f59ba0c5
badis <badis.khalsi@esprit.tn>
2022-06-24 09:58:54 +0100

    packages done

---
+++ src/main/java/com/example/backEndProject/Repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.backEndProject.Repositories;
+
+public interface UserRepository {
+}
---
+++ src/main/java/com/example/backEndProject/Services/UserService.java
@@ -0,0 +1,4 @@
+package com.example.backEndProject.Services;
+
+public interface UserService {
+}
